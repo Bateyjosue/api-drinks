7892ffd51eca5658d1fc07f22774014189399a51
Batey Josue <josuebatey19@gmail.com>
2022-05-07 18:03:16 +0200

    update endpoints

--- drinks/drinks/urls.py
+++ drinks/drinks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from drinks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('drinks/', views.drink_list, name='drinks'),
+    path('drinks/<int:id>', views.drink_detail, name='drink'),
 ]
