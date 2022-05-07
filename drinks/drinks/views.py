d51536e017a5553d7ca4b0638f9f3532cc36558d
Batey Josue <josuebatey19@gmail.com>
2022-05-07 18:01:08 +0200

    Add get drink list endpoint and drick detail endpoint, post endpoint as

---
+++ drinks/drinks/views.py
@@ -0,0 +1,40 @@
+from django.http import JsonResponse
+from .models import Drinks
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+def jls_extract_def(status):
+    return status
+
+
+@api_view(['GET', 'POST'])    
+def drink_list(request):
+    if request.method == 'GET':
+        drinks = Drinks.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({'drinks' : serializer.data})
+
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+    try:
+        drink = Drinks.objects.get(id = id)
+    except Drinks.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+        
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        pass
+
+    if request.method == 'DELETE':
+        pass
