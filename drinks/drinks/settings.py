9f8e091f0bffe0ca43c8b612437cf014f2987c73
Batey Josue <josuebatey19@gmail.com>
2022-05-07 18:03:33 +0200

    update setting

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'drinks',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
