214cf8023131e9701bf68cefeb7ed5c8caa57147
Batey Josue <josuebatey19@gmail.com>
2022-05-09 01:46:11 +0200

    url to endpoint category

--- drinks/drinks/urls.py
+++ drinks/drinks/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from drinks import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('drinks/', views.drink_list, name='drinks'),
