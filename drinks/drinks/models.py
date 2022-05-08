d070daf97d1413328c61a0189c79b11ac14b28b2
Batey Josue <josuebatey19@gmail.com>
2022-05-09 01:44:14 +0200

    update model, add category key in drinks

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Drinks(models.Model):
-    name        = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    created     = models.DateTimeField(auto_now_add=True)
-    updated     = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.name
-
 class Category(models.Model):
     name        = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     created     = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+class Drinks(models.Model):
+    category    = models.ForeignKey(Category, on_delete = models.CASCADE)
+    name        = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.name
