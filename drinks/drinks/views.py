cec54fa9e477a8079af419468cf402af790b3ad7
Batey Josue <josuebatey19@gmail.com>
2022-05-08 19:16:00 +0200

    update views

--- drinks/drinks/views.py
+++ drinks/drinks/views.py
@@ -28,13 +28,17 @@ def drink_detail(request, id):
         drink = Drinks.objects.get(id = id)
     except Drinks.DoesNotExist:
         return Response(status = status.HTTP_404_NOT_FOUND)
-        
+
     if request.method == 'GET':
         serializer = DrinkSerializer(drink)
         return Response(serializer.data)
 
     if request.method == 'PUT':
-        pass
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_404_BAD_REQUEST)
 
     if request.method == 'DELETE':
-        pass
+        drink.delete()
