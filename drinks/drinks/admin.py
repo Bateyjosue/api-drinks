5ace77fea1619fccb880605de8554403f6fe17d9
Batey Josue <josuebatey19@gmail.com>
2022-05-09 01:44:49 +0200

    register the category module

--- drinks/drinks/admin.py
+++ drinks/drinks/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Drinks
+from .models import Drinks, Category
 
 @admin.register(Drinks)
 class DrinkAdmin(admin.ModelAdmin):
-    list_display = ('pk','name', 'description')
+    list_display = ('pk','category','name', 'description', 'created')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description','created')
