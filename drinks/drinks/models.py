b832a95e991149acf7fa826d560312ee1e5f37e1
Batey Josue <josuebatey19@gmail.com>
2022-05-09 00:31:52 +0200

    add category model

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -3,8 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Drinks(models.Model):
-    name = models.CharField(max_length=200)
+    name        = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name        = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
