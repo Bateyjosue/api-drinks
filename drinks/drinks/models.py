4eab8799c8442d32b30eeb87224388a95293ee41
Batey Josue <josuebatey19@gmail.com>
2022-05-08 19:14:37 +0200

    update models

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Drinks(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+    
     def __str__(self):
         return self.name
