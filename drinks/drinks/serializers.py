c53d192c89689b21b6127de08d6f82c23518878f
Batey Josue <josuebatey19@gmail.com>
2022-05-10 22:29:46 +0200

    Validate drink name

--- drinks/drinks/serializers.py
+++ drinks/drinks/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from .models import Drinks, Category
 
+
 class DrinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drinks
-        fields = ['id','category', 'name', 'description', 'created']
+        fields = ['id', 'category', 'name', 'description', 'created']
+
+    def validate_drink_name(self, drink_name):
+        if Drinks.objects.filter(drink=drink_name).exists():
+            raise serializers.ValidationError('Drink Already exists')
+        return drink_name
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id','name', 'description', 'created']
+        fields = ['id', 'name', 'description', 'created']
